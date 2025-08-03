c28a61bf4b9da78cb294a148aa2e034b4a65a5b5
Raghugithub9 <113247392+Raghugithub9@users.noreply.github.com>
2025-08-03 22:38:05 +0530

    Update TestRun.java

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions
 	(
-	features={".//Features/login2.feature",".//Features/login.feature"},
+	features={".//Features/login2.feature"},
 	glue="stepDefinitions",
 	tags = {"@Regression"},
 	dryRun=false,
